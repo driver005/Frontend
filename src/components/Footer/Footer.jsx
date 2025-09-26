b81d5167b9edf032914217ec3cf41eaf9788d0c7
Aii <dark@dark-lord.de>
2025-09-26 17:29:03 +0200

    first impressum

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaLinkedin } from 'react
 import {
     FooterContainer,
     WebsiteRights,
-    SocialIcons,
+    TextWrap,
     SocialIconLink,
     SocialLogo,
     SocialMedia,
@@ -14,35 +14,18 @@ const Footer = () => {
     return (
         <FooterContainer>
             <SocialMedia>
+                <TextWrap>
+                    <h1>Impressum:</h1>
+                    <p>Felipe Fernandez</p>
+                    <p>Derendinger Allee 8</p>
+                    <p>72072 Tübingen</p>
+                    <p>Telefon bitte melden</p>
+                </TextWrap>
                 <SocialMediaWrap>
                     <SocialLogo to="/">
                         TECLAB
                     </SocialLogo>
-                    <WebsiteRights>teclab © 2020</WebsiteRights>
-                    <SocialIcons>
-                        <SocialIconLink href='/facebook' target="_blank" aria-label="Facebook">
-                            <FaFacebook />
-                        </SocialIconLink>
-                        <SocialIconLink href='/instagram' target="_blank" aria-label="Instagram">
-                            <FaInstagram />
-                        </SocialIconLink>
-                        <SocialIconLink
-                            href={
-                                '/youtube'
-                            }
-                            rel='noopener noreferrer'
-                            target='_blank'
-                            aria-label='Youtube'
-                        >
-                            <FaYoutube />
-                        </SocialIconLink>
-                        <SocialIconLink href='/twitter' target="_blank" aria-label="Twitter">
-                            <FaTwitter />
-                        </SocialIconLink>
-                        <SocialIconLink href='/linkedin' target="_blank" aria-label="Linkedin">
-                            <FaLinkedin />
-                        </SocialIconLink>
-                    </SocialIcons>
+                    <WebsiteRights>teclab © 2025</WebsiteRights>
                 </SocialMediaWrap>
             </SocialMedia>
         </FooterContainer>
