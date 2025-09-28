105893689a04a77ef69e8f430c2b697271b05259
driver005 <github@dark-lord.de>
2025-09-28 03:11:59 +0200

    add compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,39 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteCompression from 'vite-plugin-compression';
 import { r3f } from "@react-three/editor/vite"
 import { env } from 'process'
 //r3f()
 
 export default defineConfig({
-    plugins: [env.command === 'build' ? react() : react()],
+  plugins: [
+    env.command === 'build' ?
+      react() :
+      react(),
+    viteCompression({
+      algorithm: 'brotliCompress', // or 'gzip'
+      ext: '.br',
+    }),],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    sourcemap: false,
+    cssCodeSplit: true,
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  }
 })
