fd66506fd14ef9a563188b36824e7d728c398a10
driver005 <github@dark-lord.de>
2025-09-28 14:38:31 +0200

    impressum

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -10,25 +10,28 @@ import {
     SocialMediaWrap,
     Email,
 } from './Footer.elements'
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+    const { t } = useTranslation();
+
     return (
         <FooterContainer>
             <SocialMedia>
                 <TextWrap>
-                    <h3>Impressum:</h3>
+                    <h3>{t(`notice.title`)}</h3>
                     <p>teclab - VDI Schülerforschungszentrum</p>
                     <p>Felipe Fernández</p>
                     <p>Derendinger Allee 8</p>
                     <p>72072 Tübingen</p>
-                    <p>Phone: +491634363720</p>
+                    <p>{t(`notice.phone`)} +491634363720</p>
                     <p>Email: <Email href="mailto:sfz.teclab@vdi.de">sfz.teclab@vdi.de</Email></p>
                 </TextWrap>
-            <SocialMediaWrap>
-              <SocialLogo>
-                <IMG loading="lazy" height='35px' src="teclab.svg" />
-              </SocialLogo>
-              <WebsiteRights>teclab © 2025</WebsiteRights>
+                <SocialMediaWrap>
+                    <SocialLogo>
+                        <IMG loading="lazy" height='35px' src="teclab.svg" />
+                    </SocialLogo>
+                    <WebsiteRights>teclab © 2025</WebsiteRights>
                 </SocialMediaWrap>
             </SocialMedia>
         </FooterContainer>
